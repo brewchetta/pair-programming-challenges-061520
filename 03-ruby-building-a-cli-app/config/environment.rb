8efe7f03eb6cee9819af23df0c458621f7daee79
brewchetta <chett.tiller@gmail.com>
2020-07-20 11:12:21 -0400

    03-ruby-building-a-cli-app/

---
+++ 03-ruby-building-a-cli-app/config/environment.rb
@@ -0,0 +1,12 @@
+# Gems
+require "pry"
+require "json"
+
+# Models
+require_relative "../lib/models/Report.rb"
+
+# Services
+require_relative "../lib/services/APIService.rb"
+
+# CLI
+require_relative "../lib/CLI.rb"
---
+++ 03-ruby-building-a-cli-app/lib/CLI.rb
@@ -0,0 +1,9 @@
+class CLI
+
+  def run
+    # Add code here for your CLI
+    puts "Is your CLI running? You'd better catch it!"
+
+  end
+
+end

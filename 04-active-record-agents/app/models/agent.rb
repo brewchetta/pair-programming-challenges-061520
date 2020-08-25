953a8c68c689c0ba7de6c7d00ec2b82880758240
brewchetta <chett.tiller@gmail.com>
2020-08-25 13:32:23 -0400

    Solutions for agent

--- 04-active-record-agents/app/models/agent.rb
+++ 04-active-record-agents/app/models/agent.rb
@@ -1,4 +1,29 @@
 # In order for this to work, Agent needs to inherit from something...
 class Agent < ActiveRecord::Base
   belongs_to :organization
+
+  def fellow_agents
+    Agent.all.select{ |agent| agent.organization_id == self.organization_id }
+  end
+
+  def self.names
+    self.all.map(&:name)
+  end
+
+  def activate
+    self.update(active_status: true)
+  end
+
+  def self.active_agents
+    Agent.all.select(&:active_status)
+  end
+
+  def defect(org)
+    self.update(organization: org)
+  end
+
+  def self.rogue_agents
+    self.all.select { |agent| !agent.organization }
+  end
+
 end
